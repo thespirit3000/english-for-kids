d36815ed0bb261f43b0470c0dcd65335b6dd29f7
DzianisBogdan <thespirit3000@gmail.com>
2020-04-11 20:46:13 +0300

    feat: eslint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+  CleanWebpackPlugin,
+} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
@@ -9,60 +11,63 @@ module.exports = (env, options) => {
   const isProduction = options.mode === 'production';
 
   const config = {
-    mode: isProduction? 'production' : 'development',
-    devtool: isProduction? 'none' : 'source-map',
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
     watch: !isProduction,
     entry: ['./src/index.js', './src/sass/style.scss'],
     output: {
       path: path.join(__dirname, '/dist'),
-      filename: 'script.js'
+      filename: 'script.js',
     },
     module: {
-      rules: [
-        {
-          test: /\.html$/i,
-          loader: 'html-loader',
-        },
-        {
-          test: /\.s[ac]ss$/i,
+      rules: [{
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader',
-        ]
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        use: [{
+          loader: 'file-loader',
+        }],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-        {
-          test: /\.(png|svg|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-            },
-          ],
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
-      ]
+      },
+      ],
     },
 
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        template: 'index.html'
+        template: 'index.html',
       }),
       new MiniCssExtractPlugin({
-        filename: 'style.css'
+        filename: 'style.css',
       }),
     ],
-  }
+  };
 
-  
-    return config;
-}
+
+  return config;
+};
