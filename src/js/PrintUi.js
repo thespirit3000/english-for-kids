d00b458e4ba52f0cd2af3534d0d62e116f744fb8
DzianisBogdan <thespirit3000@gmail.com>
2020-04-11 23:10:25 +0300

    feat: ui html class

---
+++ src/js/PrintUi.js
@@ -0,0 +1,13 @@
+export default class PrintUi {
+  constructor() {
+    this.header = `<div class='wrapper'>
+                      <div class='header'>
+                        <button class="btn"><i class="fa fa-bars"></i></button>
+                        <label class="switch">
+                          <input type="checkbox">
+                          <span class="slider round">RUUJN</span>
+                        </label>
+                      </div>
+                    </div>`;
+  }
+}
