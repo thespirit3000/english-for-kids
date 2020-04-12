d039250fe6a752fde1009f19bfa3a31b6041101e
DzianisBogdan <thespirit3000@gmail.com>
2020-04-12 19:35:57 +0300

    feat: assets add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import PrintUi from './js/PrintUi';
 import interfaceTemplate from './js/interface';
 import eventHandler from './js/eventHandler';
+import renderCategoryMenu from './js/render';
 
 const initialize = () => {
   const header = new PrintUi(interfaceTemplate.header);
   const footer = new PrintUi(interfaceTemplate.footer);
+  const main = new PrintUi(interfaceTemplate.main);
+  const menu = new PrintUi(interfaceTemplate.side_panel);
   header.printHTML(document.body);
+  main.printHTML(document.body);
   footer.printHTML(document.body);
+  renderCategoryMenu();
+  menu.printHTML(document.body);
   eventHandler();
 };
 
