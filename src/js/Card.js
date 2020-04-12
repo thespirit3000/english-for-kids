fac971dfc9939f25e78011d3e3df6b337ebbb2b7
DzianisBogdan <thespirit3000@gmail.com>
2020-04-12 21:38:40 +0300

    feat: flipcard

---
+++ src/js/Card.js
@@ -0,0 +1,29 @@
+export default class Category {
+  constructor(dataObj) {
+    this.word = dataObj.word;
+    this.translation = dataObj.translation;
+    this.imgUrl = dataObj.image;
+    this.audioSrc = dataObj.audioSrc;
+  }
+
+  renderCard() {
+    return `
+    <div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+        <img src="./assets/${this.imgUrl}" alt="Imagine image category" class='card-img data-card'">
+        <div class="word-container">
+          <h4 class='data-card'><b>${this.word}</b></h4>
+        </div>
+      </div>
+      <div class="flip-card-back">
+      
+      <img src="./assets/${this.imgUrl}" alt="Imagine image category" class='card-img data-card'">
+      <div class="word-container">
+        <h4 class='data-card'><b>${this.translation}</b></h4>
+      </div>
+    </div>
+  </div>
+  `;
+  }
+}
