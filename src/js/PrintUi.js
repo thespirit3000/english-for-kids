434dd113f73db82a6e1cf313e6a894a5f462451c
DzianisBogdan <thespirit3000@gmail.com>
2020-04-12 10:46:44 +0300

    feat: insert template class

--- src/js/PrintUi.js
+++ src/js/PrintUi.js
@@ -1,17 +1,9 @@
 export default class PrintUi {
-  constructor() {
-    this.header = `
-    <section class=header_section>
-      <div class='wrapper'>
-        <div class='header'>
-          <button class="btn menu_btn"><i class="fa fa-bars"></i></button>
-          <label class="switch">
-          <input class="switcher" type="checkbox">
-          <span class="slider round"></span>
-          </label>
-        </div>
-      </div>
-    </section>
-                    `;
+  constructor(template) {
+    this.template = template;
+  }
+
+  printHTML(node) {
+    node.insertAdjacentHTML('beforeend', this.template);
   }
 }
