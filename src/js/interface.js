04ed2f856fa541ec761f6430793b0698a9892028
DzianisBogdan <thespirit3000@gmail.com>
2020-04-12 19:39:55 +0300

    feat: side panel

--- src/js/interface.js
+++ src/js/interface.js
@@ -3,7 +3,7 @@ export default {
     <section class=header_section>
       <div class='wrapper'>
         <div class='header'>
-          <button class="btn menu_btn"><i class="fa fa-bars"></i></button>
+          <button class="btn menu_btn open_menu"><i class="fa fa-bars open_menu"></i></button>
           <label class="switch">
           <input class="switcher" type="checkbox">
           <span class="slider round"></span>
@@ -11,7 +11,23 @@ export default {
         </div>
       </div>
     </section>
-                    `,
+  `,
+  side_panel: `<div id="mainMenu" class="sidepanel">
+  <button class="btn btn_close close_menu"><i class="fa fa-close close_menu"></i></button>
+  <a href="#">About</a> 
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+  </div>`,
+  main: `
+    <section class="main_section">
+      <div class='wrapper'>
+        <div class="main main-window">
+          
+        </div>
+      </div> 
+    </section> 
+  `,
   footer: `
     <section class="footer_section">
       <div class='wrapper'>
