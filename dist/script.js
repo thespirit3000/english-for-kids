74747d492213c34a1d68b2c0093eeec16ffb2413
DzianisBogdan <thespirit3000@gmail.com>
2020-04-12 10:44:55 +0300

    refactor: dist deploy

--- dist/script.js
+++ dist/script.js
@@ -96,35 +96,18 @@
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _js_PrintUi__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/PrintUi */ "./src/js/PrintUi.js");
+/* harmony import */ var _js_interface__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/interface */ "./src/js/interface.js");
+/* harmony import */ var _js_eventHandler__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./js/eventHandler */ "./src/js/eventHandler.js");
 
 
-var gameModeSet = function gameModeSet(gameSwitcher) {
-  if (gameSwitcher.checked === true) {
-    console.log('game');
-  } else {
-    console.log('learn');
-  }
-};
 
-var handleClick = function handleClick(event) {
-  return 1;
-};
-
-var handleInput = function handleInput(event) {
-  if (event.target.classList.contains('switcher')) {
-    gameModeSet(event.target);
-  }
-};
-
-var eventHandler = function eventHandler() {
-  document.addEventListener('input', handleInput);
-  document.addEventListener('click', handleClick);
-};
 
 var initialize = function initialize() {
-  var ui = new _js_PrintUi__WEBPACK_IMPORTED_MODULE_0__["default"]();
-  document.body.insertAdjacentHTML('afterbegin', ui.header);
-  eventHandler();
+  var header = new _js_PrintUi__WEBPACK_IMPORTED_MODULE_0__["default"](_js_interface__WEBPACK_IMPORTED_MODULE_1__["default"].header);
+  var footer = new _js_PrintUi__WEBPACK_IMPORTED_MODULE_0__["default"](_js_interface__WEBPACK_IMPORTED_MODULE_1__["default"].footer);
+  header.printHTML(document.body);
+  footer.printHTML(document.body);
+  Object(_js_eventHandler__WEBPACK_IMPORTED_MODULE_2__["default"])();
 };
 
 var unloadWindow = function unloadWindow() {};
@@ -146,10 +129,95 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return PrintUi; });
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var PrintUi = function PrintUi() {
-  _classCallCheck(this, PrintUi);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-  this.header = "\n    <section class=header_section>\n      <div class='wrapper'>\n        <div class='header'>\n          <button class=\"btn menu_btn\"><i class=\"fa fa-bars\"></i></button>\n          <label class=\"switch\">\n          <input class=\"switcher\" type=\"checkbox\">\n          <span class=\"slider round\"></span>\n          </label>\n        </div>\n      </div>\n    </section>\n                    ";
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var PrintUi = /*#__PURE__*/function () {
+  function PrintUi(template) {
+    _classCallCheck(this, PrintUi);
+
+    this.template = template;
+  }
+
+  _createClass(PrintUi, [{
+    key: "printHTML",
+    value: function printHTML(node) {
+      node.insertAdjacentHTML('beforeend', this.template);
+    }
+  }]);
+
+  return PrintUi;
+}();
+
+
+
+/***/ }),
+
+/***/ "./src/js/eventHandler.js":
+/*!********************************!*\
+  !*** ./src/js/eventHandler.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return eventHandler; });
+/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
+
+
+var handleClick = function handleClick(event) {
+  return 1;
+};
+
+var handleInput = function handleInput(event) {
+  if (event.target.classList.contains('switcher')) {
+    Object(_utils__WEBPACK_IMPORTED_MODULE_0__["default"])(event.target);
+  }
+};
+
+var eventHandler = function eventHandler() {
+  document.addEventListener('input', handleInput);
+  document.addEventListener('click', handleClick);
+};
+
+
+
+/***/ }),
+
+/***/ "./src/js/interface.js":
+/*!*****************************!*\
+  !*** ./src/js/interface.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ({
+  header: "\n    <section class=header_section>\n      <div class='wrapper'>\n        <div class='header'>\n          <button class=\"btn menu_btn\"><i class=\"fa fa-bars\"></i></button>\n          <label class=\"switch\">\n          <input class=\"switcher\" type=\"checkbox\">\n          <span class=\"slider round\"></span>\n          </label>\n        </div>\n      </div>\n    </section>\n                    ",
+  footer: "\n    <section class=\"footer_section\">\n      <div class='wrapper'>\n        <div class=\"footer\">\n          <a href=\"mailto:thespirit3000@gmail.com\">created by Dzianis Bogdan</>\n        </div>\n      </div>  \n    </section>\n  "
+});
+
+/***/ }),
+
+/***/ "./src/js/utils.js":
+/*!*************************!*\
+  !*** ./src/js/utils.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return gameModeSet; });
+var gameModeSet = function gameModeSet(gameSwitcher) {
+  if (gameSwitcher.checked === true) {
+    console.log('game');
+  } else {
+    console.log('learn');
+  }
 };
 
 
