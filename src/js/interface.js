281a78f23da4644587c1fe74b62415a04cb7d543
DzianisBogdan <thespirit3000@gmail.com>
2020-04-12 10:46:08 +0300

    feat: template obj

---
+++ src/js/interface.js
@@ -0,0 +1,24 @@
+export default {
+  header: `
+    <section class=header_section>
+      <div class='wrapper'>
+        <div class='header'>
+          <button class="btn menu_btn"><i class="fa fa-bars"></i></button>
+          <label class="switch">
+          <input class="switcher" type="checkbox">
+          <span class="slider round"></span>
+          </label>
+        </div>
+      </div>
+    </section>
+                    `,
+  footer: `
+    <section class="footer_section">
+      <div class='wrapper'>
+        <div class="footer">
+          <a href="mailto:thespirit3000@gmail.com">created by Dzianis Bogdan</>
+        </div>
+      </div>  
+    </section>
+  `,
+};
