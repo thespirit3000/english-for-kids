847ae1ee5317a3575f7857208e4d01ab6bc47771
DzianisBogdan <thespirit3000@gmail.com>
2020-04-12 08:12:33 +0300

    refactor: header layout

--- src/js/PrintUi.js
+++ src/js/PrintUi.js
@@ -1,13 +1,17 @@
 export default class PrintUi {
   constructor() {
-    this.header = `<div class='wrapper'>
-                      <div class='header'>
-                        <button class="btn"><i class="fa fa-bars"></i></button>
-                        <label class="switch">
-                          <input type="checkbox">
-                          <span class="slider round">RUUJN</span>
-                        </label>
-                      </div>
-                    </div>`;
+    this.header = `
+    <section class=header_section>
+      <div class='wrapper'>
+        <div class='header'>
+          <button class="btn menu_btn"><i class="fa fa-bars"></i></button>
+          <label class="switch">
+          <input class="switcher" type="checkbox">
+          <span class="slider round"></span>
+          </label>
+        </div>
+      </div>
+    </section>
+                    `;
   }
 }
