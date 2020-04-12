ee166e45faf366424fc76bf7bfe222b461607bce
DzianisBogdan <thespirit3000@gmail.com>
2020-04-12 19:37:02 +0300

    refactor: dist deploy

--- dist/script.js
+++ dist/script.js
@@ -98,6 +98,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _js_PrintUi__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/PrintUi */ "./src/js/PrintUi.js");
 /* harmony import */ var _js_interface__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/interface */ "./src/js/interface.js");
 /* harmony import */ var _js_eventHandler__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./js/eventHandler */ "./src/js/eventHandler.js");
+/* harmony import */ var _js_render__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./js/render */ "./src/js/render.js");
+
 
 
 
@@ -110,6 +112,7 @@ var initialize = function initialize() {
   header.printHTML(document.body);
   main.printHTML(document.body);
   footer.printHTML(document.body);
+  Object(_js_render__WEBPACK_IMPORTED_MODULE_3__["default"])();
   menu.printHTML(document.body);
   Object(_js_eventHandler__WEBPACK_IMPORTED_MODULE_2__["default"])();
 };
@@ -118,6 +121,44 @@ var unloadWindow = function unloadWindow() {};
 
 window.onload = initialize;
 window.onbeforeunload = unloadWindow;
+
+/***/ }),
+
+/***/ "./src/js/Category.js":
+/*!****************************!*\
+  !*** ./src/js/Category.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Category; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Category = /*#__PURE__*/function () {
+  function Category(categoryName, imgUrl) {
+    _classCallCheck(this, Category);
+
+    this.categoryName = categoryName;
+    this.imgUrl = imgUrl;
+  }
+
+  _createClass(Category, [{
+    key: "renderCard",
+    value: function renderCard() {
+      return "\n        <div id=\"".concat(this.categoryName, "\" class=\"card\">\n        <img src=\"").concat(this.imgUrl, "\" alt=\"Here was might img\" class='card-img'\">\n        <div class=\"container\">\n          <h4><b>").concat(this.categoryName, "</b></h4>\n        </div>\n      </div>\n        ");
+    }
+  }]);
+
+  return Category;
+}();
+
+
 
 /***/ }),
 
@@ -158,6 +199,340 @@ var PrintUi = /*#__PURE__*/function () {
 
 /***/ }),
 
+/***/ "./src/js/cards.js":
+/*!*************************!*\
+  !*** ./src/js/cards.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var cards = [['Action (set A)', 'Action (set B)', 'Animal (set A)', 'Animal (set B)', 'Clothes', 'Emotions', 'Toys', 'Food'], [{
+  word: 'cry',
+  translation: 'плакать',
+  image: 'img/cry.jpg',
+  audioSrc: 'audio/cry.mp3'
+}, {
+  word: 'dance',
+  translation: 'танцевать',
+  image: 'img/dance.jpg',
+  audioSrc: 'audio/dance.mp3'
+}, {
+  word: 'dive',
+  translation: 'нырять',
+  image: 'img/dive.jpg',
+  audioSrc: 'audio/dive.mp3'
+}, {
+  word: 'draw',
+  translation: 'рисовать',
+  image: 'img/draw.jpg',
+  audioSrc: 'audio/draw.mp3'
+}, {
+  word: 'fish',
+  translation: 'ловить рыбу',
+  image: 'img/fish.jpg',
+  audioSrc: 'audio/fish.mp3'
+}, {
+  word: 'fly',
+  translation: 'летать',
+  image: 'img/fly.jpg',
+  audioSrc: 'audio/fly.mp3'
+}, {
+  word: 'hug',
+  translation: 'обнимать',
+  image: 'img/hug.jpg',
+  audioSrc: 'audio/hug.mp3'
+}, {
+  word: 'jump',
+  translation: 'прыгать',
+  image: 'img/jump.jpg',
+  audioSrc: 'audio/jump.mp3'
+}], [{
+  word: 'open',
+  translation: 'открывать',
+  image: 'img/open.jpg',
+  audioSrc: 'audio/open.mp3'
+}, {
+  word: 'play',
+  translation: 'играть',
+  image: 'img/play.jpg',
+  audioSrc: 'audio/play.mp3'
+}, {
+  word: 'point',
+  translation: 'указывать',
+  image: 'img/point.jpg',
+  audioSrc: 'audio/point.mp3'
+}, {
+  word: 'ride',
+  translation: 'ездить',
+  image: 'img/ride.jpg',
+  audioSrc: 'audio/ride.mp3'
+}, {
+  word: 'run',
+  translation: 'бегать',
+  image: 'img/run.jpg',
+  audioSrc: 'audio/run.mp3'
+}, {
+  word: 'sing',
+  translation: 'петь',
+  image: 'img/sing.jpg',
+  audioSrc: 'audio/sing.mp3'
+}, {
+  word: 'skip',
+  translation: 'пропускать, прыгать',
+  image: 'img/skip.jpg',
+  audioSrc: 'audio/skip.mp3'
+}, {
+  word: 'swim',
+  translation: 'плавать',
+  image: 'img/swim.jpg',
+  audioSrc: 'audio/swim.mp3'
+}], [{
+  word: 'cat',
+  translation: 'кот',
+  image: 'img/cat.jpg',
+  audioSrc: 'audio/cat.mp3'
+}, {
+  word: 'chick',
+  translation: 'цыплёнок',
+  image: 'img/chick.jpg',
+  audioSrc: 'audio/chick.mp3'
+}, {
+  word: 'chicken',
+  translation: 'курица',
+  image: 'img/chicken.jpg',
+  audioSrc: 'audio/chicken.mp3'
+}, {
+  word: 'dog',
+  translation: 'собака',
+  image: 'img/dog.jpg',
+  audioSrc: 'audio/dog.mp3'
+}, {
+  word: 'horse',
+  translation: 'лошадь',
+  image: 'img/horse.jpg',
+  audioSrc: 'audio/horse.mp3'
+}, {
+  word: 'pig',
+  translation: 'свинья',
+  image: 'img/pig.jpg',
+  audioSrc: 'audio/pig.mp3'
+}, {
+  word: 'rabbit',
+  translation: 'кролик',
+  image: 'img/rabbit.jpg',
+  audioSrc: 'audio/rabbit.mp3'
+}, {
+  word: 'sheep',
+  translation: 'овца',
+  image: 'img/sheep.jpg',
+  audioSrc: 'audio/sheep.mp3'
+}], [{
+  word: 'bird',
+  translation: 'птица',
+  image: 'img/bird.jpg',
+  audioSrc: 'audio/bird.mp3'
+}, {
+  word: 'fish',
+  translation: 'рыба',
+  image: 'img/fish1.jpg',
+  audioSrc: 'audio/fish.mp3'
+}, {
+  word: 'frog',
+  translation: 'жаба',
+  image: 'img/frog.jpg',
+  audioSrc: 'audio/frog.mp3'
+}, {
+  word: 'giraffe',
+  translation: 'жирафа',
+  image: 'img/giraffe.jpg',
+  audioSrc: 'audio/giraffe.mp3'
+}, {
+  word: 'lion',
+  translation: 'лев',
+  image: 'img/lion.jpg',
+  audioSrc: 'audio/lion.mp3'
+}, {
+  word: 'mouse',
+  translation: 'мышь',
+  image: 'img/mouse.jpg',
+  audioSrc: 'audio/mouse.mp3'
+}, {
+  word: 'turtle',
+  translation: 'черепаха',
+  image: 'img/turtle.jpg',
+  audioSrc: 'audio/turtle.mp3'
+}, {
+  word: 'dolphin',
+  translation: 'дельфин',
+  image: 'img/dolphin.jpg',
+  audioSrc: 'audio/dolphin.mp3'
+}], [{
+  word: 'skirt',
+  translation: 'юбка',
+  image: 'img/skirt.jpg',
+  audioSrc: 'audio/skirt.mp3'
+}, {
+  word: 'pants',
+  translation: 'брюки',
+  image: 'img/pants.jpg',
+  audioSrc: 'audio/pants.mp3'
+}, {
+  word: 'blouse',
+  translation: 'блузка',
+  image: 'img/blouse.jpg',
+  audioSrc: 'audio/blouse.mp3'
+}, {
+  word: 'dress',
+  translation: 'платье',
+  image: 'img/dress.jpg',
+  audioSrc: 'audio/dress.mp3'
+}, {
+  word: 'boot',
+  translation: 'ботинок',
+  image: 'img/boot.jpg',
+  audioSrc: 'audio/boot.mp3'
+}, {
+  word: 'shirt',
+  translation: 'рубашка',
+  image: 'img/shirt.jpg',
+  audioSrc: 'audio/shirt.mp3'
+}, {
+  word: 'coat',
+  translation: 'пальто',
+  image: 'img/coat.jpg',
+  audioSrc: 'audio/coat.mp3'
+}, {
+  word: 'shoe',
+  translation: 'туфли',
+  image: 'img/shoe.jpg',
+  audioSrc: 'audio/shoe.mp3'
+}], [{
+  word: 'sad',
+  translation: 'грустный',
+  image: 'img/sad.jpg',
+  audioSrc: 'audio/sad.mp3'
+}, {
+  word: 'angry',
+  translation: 'сердитый',
+  image: 'img/angry.jpg',
+  audioSrc: 'audio/angry.mp3'
+}, {
+  word: 'happy',
+  translation: 'счастливый',
+  image: 'img/happy.jpg',
+  audioSrc: 'audio/happy.mp3'
+}, {
+  word: 'tired',
+  translation: 'уставший',
+  image: 'img/tired.jpg',
+  audioSrc: 'audio/tired.mp3'
+}, {
+  word: 'surprised',
+  translation: 'удивлённый',
+  image: 'img/surprised.jpg',
+  audioSrc: 'audio/surprised.mp3'
+}, {
+  word: 'scared',
+  translation: 'испуганный',
+  image: 'img/scared.jpg',
+  audioSrc: 'audio/scared.mp3'
+}, {
+  word: 'smile',
+  translation: 'улыбка',
+  image: 'img/smile.jpg',
+  audioSrc: 'audio/smile.mp3'
+}, {
+  word: 'laugh',
+  translation: 'смех',
+  image: 'img/laugh.jpg',
+  audioSrc: 'audio/laugh.mp3'
+}], [{
+  word: 'sad',
+  translation: 'грустный',
+  image: 'img/sad.jpg',
+  audioSrc: 'audio/sad.mp3'
+}, {
+  word: 'angry',
+  translation: 'сердитый',
+  image: 'img/angry.jpg',
+  audioSrc: 'audio/angry.mp3'
+}, {
+  word: 'happy',
+  translation: 'счастливый',
+  image: 'img/happy.jpg',
+  audioSrc: 'audio/happy.mp3'
+}, {
+  word: 'tired',
+  translation: 'уставший',
+  image: 'img/tired.jpg',
+  audioSrc: 'audio/tired.mp3'
+}, {
+  word: 'surprised',
+  translation: 'удивлённый',
+  image: 'img/surprised.jpg',
+  audioSrc: 'audio/surprised.mp3'
+}, {
+  word: 'scared',
+  translation: 'испуганный',
+  image: 'img/scared.jpg',
+  audioSrc: 'audio/scared.mp3'
+}, {
+  word: 'smile',
+  translation: 'улыбка',
+  image: 'img/smile.jpg',
+  audioSrc: 'audio/smile.mp3'
+}, {
+  word: 'laugh',
+  translation: 'смех',
+  image: 'img/laugh.jpg',
+  audioSrc: 'audio/laugh.mp3'
+}], [{
+  word: 'sad',
+  translation: 'грустный',
+  image: 'img/sad.jpg',
+  audioSrc: 'audio/sad.mp3'
+}, {
+  word: 'angry',
+  translation: 'сердитый',
+  image: 'img/angry.jpg',
+  audioSrc: 'audio/angry.mp3'
+}, {
+  word: 'happy',
+  translation: 'счастливый',
+  image: 'img/happy.jpg',
+  audioSrc: 'audio/happy.mp3'
+}, {
+  word: 'tired',
+  translation: 'уставший',
+  image: 'img/tired.jpg',
+  audioSrc: 'audio/tired.mp3'
+}, {
+  word: 'surprised',
+  translation: 'удивлённый',
+  image: 'img/surprised.jpg',
+  audioSrc: 'audio/surprised.mp3'
+}, {
+  word: 'scared',
+  translation: 'испуганный',
+  image: 'img/scared.jpg',
+  audioSrc: 'audio/scared.mp3'
+}, {
+  word: 'smile',
+  translation: 'улыбка',
+  image: 'img/smile.jpg',
+  audioSrc: 'audio/smile.mp3'
+}, {
+  word: 'laugh',
+  translation: 'смех',
+  image: 'img/laugh.jpg',
+  audioSrc: 'audio/laugh.mp3'
+}]];
+/* harmony default export */ __webpack_exports__["default"] = (cards);
+
+/***/ }),
+
 /***/ "./src/js/eventHandler.js":
 /*!********************************!*\
   !*** ./src/js/eventHandler.js ***!
@@ -172,12 +547,12 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var handleClick = function handleClick(event) {
-  if (event.target.classList.contains('menu_btn')) {
+  if (event.target.classList.contains('open_menu')) {
     var menuElement = document.querySelector('.sidepanel');
     menuElement.setAttribute('style', 'width: 300px');
   }
 
-  if (event.target.classList.contains('btn_close')) {
+  if (event.target.classList.contains('close_menu')) {
     var _menuElement = document.querySelector('.sidepanel');
 
     _menuElement.setAttribute('style', 'width: 0px');
@@ -209,11 +584,41 @@ var eventHandler = function eventHandler() {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
-  header: "\n    <section class=header_section>\n      <div class='wrapper'>\n        <div class='header'>\n          <button class=\"btn menu_btn\"><i class=\"fa fa-bars\"></i></button>\n          <label class=\"switch\">\n          <input class=\"switcher\" type=\"checkbox\">\n          <span class=\"slider round\"></span>\n          </label>\n        </div>\n      </div>\n    </section>\n  ",
-  side_panel: "<div id=\"mainMenu\" class=\"sidepanel\">\n  <button class=\"btn btn_close\"><i class=\"fa fa-close\"></i></button>\n  <a href=\"#\">About</a> \n  <a href=\"#\">Services</a>\n  <a href=\"#\">Clients</a>\n  <a href=\"#\">Contact</a>\n  </div>",
-  main: "\n    <section class=\"main_section\">\n      <div class='wrapper'>\n        <div class=\"main main-window\">\n          <div class=\"color_card\">\n          </div> \n          <div class=\"color_card\">\n          </div>\n          <div class=\"color_card\">\n          </div>\n          <div class=\"color_card\">\n          </div>\n          <div class=\"color_card\">\n          </div>\n          <div class=\"color_card\">\n          </div>\n          <div class=\"color_card\">\n          </div>\n          <div class=\"color_card\">\n          </div>\n        </div>\n      </div> \n    </section> \n  ",
+  header: "\n    <section class=header_section>\n      <div class='wrapper'>\n        <div class='header'>\n          <button class=\"btn menu_btn open_menu\"><i class=\"fa fa-bars open_menu\"></i></button>\n          <label class=\"switch\">\n          <input class=\"switcher\" type=\"checkbox\">\n          <span class=\"slider round\"></span>\n          </label>\n        </div>\n      </div>\n    </section>\n  ",
+  side_panel: "<div id=\"mainMenu\" class=\"sidepanel\">\n  <button class=\"btn btn_close close_menu\"><i class=\"fa fa-close close_menu\"></i></button>\n  <a href=\"#\">About</a> \n  <a href=\"#\">Services</a>\n  <a href=\"#\">Clients</a>\n  <a href=\"#\">Contact</a>\n  </div>",
+  main: "\n    <section class=\"main_section\">\n      <div class='wrapper'>\n        <div class=\"main main-window\">\n          \n        </div>\n      </div> \n    </section> \n  ",
   footer: "\n    <section class=\"footer_section\">\n      <div class='wrapper'>\n        <div class=\"footer\">\n          <a href=\"mailto:thespirit3000@gmail.com\">created by Dzianis Bogdan</>\n        </div>\n      </div>  \n    </section>\n  "
 });
+
+/***/ }),
+
+/***/ "./src/js/render.js":
+/*!**************************!*\
+  !*** ./src/js/render.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Category__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Category */ "./src/js/Category.js");
+/* harmony import */ var _cards__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./cards */ "./src/js/cards.js");
+
+
+
+var renderCategoryMenu = function renderCategoryMenu() {
+  _cards__WEBPACK_IMPORTED_MODULE_1__["default"][0].forEach(function (element) {
+    console.log(element);
+    var mainCategory = document.querySelector('.main-window');
+    var indexOfElement = _cards__WEBPACK_IMPORTED_MODULE_1__["default"][0].indexOf(element) + 1;
+    var imgUrl = "./assets/".concat(_cards__WEBPACK_IMPORTED_MODULE_1__["default"][indexOfElement][3].image);
+    console.log(imgUrl);
+    var category = new _Category__WEBPACK_IMPORTED_MODULE_0__["default"](element, imgUrl);
+    mainCategory.insertAdjacentHTML('beforeend', category.renderCard());
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (renderCategoryMenu);
 
 /***/ }),
 
