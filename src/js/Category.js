182626ab4f9f509b84a4f35ebacf35c0d90b7bac
DzianisBogdan <thespirit3000@gmail.com>
2020-04-13 23:10:28 +0300

    feat: render side menu item

--- src/js/Category.js
+++ src/js/Category.js
@@ -1,12 +1,20 @@
+
 export default class Category {
-  constructor(categoryName, imgUrl) {
+  constructor(categoryName, imgUrl, categoryNum) {
     this.categoryName = categoryName;
     this.imgUrl = imgUrl;
+    this.categoryNum = categoryNum;
+  }
+
+  renderMenu() {
+    return `
+    <div id="${this.categoryNum}" class="sidemenu-item menu-item item${this.categoryNum}">${this.categoryName}</div>
+    `;
   }
 
   renderCard() {
     return `
-        <div id="${this.categoryName}" class="card data-category">
+        <div id="${this.categoryNum}" class="card data-category">
         <img src="${this.imgUrl}" alt="Imagine image category" class='card-img data-category'">
         <div class="container data-category">
           <h4 class='data-category'><b>${this.categoryName}</b></h4>
