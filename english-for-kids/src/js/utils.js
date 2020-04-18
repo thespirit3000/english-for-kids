208c173b928c6f180a55a442b67ae6051cd934a1
DzianisBogdan <thespirit3000@gmail.com>
2020-04-19 00:31:47 +0300

    feat: utils dom functions

--- english-for-kids/src/js/utils.js
+++ english-for-kids/src/js/utils.js
@@ -1,10 +1,5 @@
-export const gameModeSet = (gameSwitcher) => {
-  if (gameSwitcher.checked === true) {
-    console.log('game');
-  } else {
-    console.log('learn');
-  }
-};
+import state from './state';
+import cards from './data-array';
 
 export const elementBySelector = (element, selector) => element.querySelector(selector);
 
@@ -25,5 +20,37 @@ export const closeMenu = () => {
   const menuElement = document.querySelector('.sidepanel');
   menuElement.setAttribute('style', 'width: 0px');
 };
+
+export const showButton = (selector) => {
+  const buttonToShow = document.querySelector(selector);
+  buttonToShow.classList.remove('hidden_opacity');
+};
+
+export const hideButton = (selector) => {
+  const buttonToHide = document.querySelector(selector);
+  buttonToHide.classList.add('hidden_opacity');
+};
+
+const shuffleDom = (parentSelector) => {
+  const list = document.querySelector(parentSelector);
+  for (let i = list.children.length; i >= 0; i--) {
+    list.appendChild(list.children[(Math.random() * i) || 0]);
+  }
+};
+
+const mainArray = () => document.querySelector('.main').children;
+
+export const startGame = () => {
+  const array = mainArray();
+  showButton('.repeat_btn');
+};
+
+export const stopGame = () => {
+  hideButton('.repeat_btn');
+  if (state.game) {
+    showButton('.game_btn');
+  }
+};
+
 
 export const removeWhiteSpaces = (string) => string.split(/\s/).join('');
